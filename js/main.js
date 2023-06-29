1bf262fa018e742d6b72ffb79f6c91f00c2b08eb
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-06-29 14:31:17 +0200

    release

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ const lastName = prompt ("Inserisciqui il tuo Cognome");
 const colour = prompt ("Inserisci qui il tuo colore preferito");
 const number = '21'
 
+// password
 const password = (firstName + lastName + colour + number);
 
 
 // console.log(password);
 
-
+//benvenuto
 document.getElementById("user-name").innerHTML = firstName;
+
+//inner-password
 document.getElementById("password-text").innerHTML = password;
 
